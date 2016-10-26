f3768473b937b16e1ddfb309e1b07e675a9eafd9
Vladislav <c00per@mail.ua>
2016-10-26 16:10:41 +0300

    test
    test

--- js/script.js
+++ js/script.js
@@ -26,6 +26,63 @@ var show = function show(event) {
 	}
 };
 
+var howerSlideSpeed = 2000;
+var howerTimeOut = 5000;
+var howerNeedLinks = true;
+
+$(document).ready(function(e){
+	$('.slide').css(
+	{
+		"position" : "absolute",
+        "top":'0', "left": '0'
+	}).hide().eq(0).show();
+	var slideNum = 0;
+	var slideTime;
+	var slideCount = $('#slider .slide').size();
+
+	var animSlide = function(arrow){
+        clearTimeout(slideTime);
+        $('.slide').eq(slideNum).fadeOut(howerSlideSpeed);
+        if(arrow == "next"){
+            if(slideNum == (slideCount-1)){slideNum=0;}
+            else{slideNum++}
+            }
+        else if(arrow == "prew")
+        {
+            if(slideNum == 0){slideNum=slideCount-1;}
+            else{slideNum-=1}
+        }
+        else{
+            slideNum = arrow;
+            }
+        $('.slide').eq(slideNum).fadeIn(howerSlideSpeed, rotator);
+    }
+
+    if(howerNeedLinks){
+var $linkArrow = $('<a id="prewbutton"></a><a id="nextbutton">&gt;</a>')
+    .prependTo('#slider');      
+    $('#nextbutton').click(function(){
+        animSlide("next");
+ 		return false;
+        })
+    $('#prewbutton').click(function(){
+        animSlide("prew");
+        return false;
+        })
+}
+    var pause = false;
+    var rotator = function(){
+    if(!pause){slideTime = setTimeout(function(){animSlide('next')}, howerTimeOut);}
+            }
+    $('#slider-wrap').hover(    
+        function(){clearTimeout(slideTime); pause = true;},
+        function(){pause = false; rotator();
+        });
+    rotator();
+});
+
 $accorHead1.on('click', show);
 $accorHead2.on('click', show);
 $accorHead3.on('click', show);
+
+
